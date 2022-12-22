0ba55ce80d4d7de0590beb4bac364a8ef9a8fb45
thyeom3891 <xhslqk2003@naver.com>
2022-12-22 22:24:40 +0900

    인프런 JAVA를 통해 생성자 주입

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,13 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    AppConfig appConfig = new AppConfig();
+    MemberService memberService = appConfig.memberService();
     @Test
     void join(){
         Member member = new Member(1L,"memberA",Grade.VIP);
